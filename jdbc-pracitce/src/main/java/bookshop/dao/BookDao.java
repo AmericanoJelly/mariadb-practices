38cc5bcae29eabead61ce96d3e0a7cadedf2cd19
YUJIN <yujinj704@gmail.com>
2022-05-19 11:14:10 +0900

    BookDao

--- jdbc-pracitce/src/main/java/bookshop/dao/BookDao.java
+++ jdbc-pracitce/src/main/java/bookshop/dao/BookDao.java
@@ -20,16 +20,12 @@ public class BookDao {
 		try {
 			connection = getConnection();
 			
-			//3. SQL 준비
 			String sql = "insert into book values(null, ?, ?, ?)";
 			pstmt = connection.prepareStatement(sql);
-			
-			//4.Mapping(bind)
 			pstmt.setString(1, vo.getTitle());
 			pstmt.setString(2, vo.getStateCode());
 			pstmt.setLong(3, vo.getAuthorNo());
 			
-			//4. SQL 실행
 			int count = pstmt.executeUpdate();
 			result = count == 1;
 		} catch (SQLException e) {
@@ -50,7 +46,6 @@ public class BookDao {
 		return result;		
 	}
 
-
 	public List<BookVo> findAll() {
 		List<BookVo> result = new ArrayList<>();
 		Connection connection = null;
@@ -59,16 +54,14 @@ public class BookDao {
 		
 		try {
 			connection = getConnection();
-			
+
 			String sql =
 				"   select a.no, a.title, b.name, a.state_code" +
 				"     from book a, author b" +
 				"    where a.author_no = b.no" +
 				" order by no asc";
-			pstmt = connection.prepareStatement(sql);
-	
+			pstmt = connection.prepareStatement(sql);			
 			rs = pstmt.executeQuery();
-			
 			
 			while(rs.next()) {
 				Long no = rs.getLong(1);
@@ -105,36 +98,107 @@ public class BookDao {
 		return result;		
 	}
 	
-	public void update(long no, String string) {
-		// TODO Auto-generated method stub
+	public void update(Long no, String stateCode) {
+		BookVo vo = new BookVo();
+		vo.setNo(no);
+		vo.setStateCode(stateCode);
 		
+		update(vo);
 	}
-	
 	
 	public boolean update(BookVo vo) {
-		
-	}
-	
-	public BookVo findByNo(long l) {
-		
-	}
-	
-	private Connection getConnection() throws SQLException{
-	
+		boolean result = false;
 		Connection connection = null;
+		PreparedStatement pstmt = null;
+		
 		try {
-			String sql = "insert into book values(null, ?, ?, ?)";
+			connection = getConnection();
+			
+			String sql = "update book set state_code=? where no=?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, vo.getStateCode());
+			pstmt.setLong(2, vo.getNo());
+			
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;		
+	}
+	
+	public BookVo findByNo(long no) {
+		BookVo result = null;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = getConnection();
+
+			String sql =
+				"select a.no, a.title, a.author_no, b.name, a.state_code" +
+				"  from book a, author b" +
+				" where a.author_no = b.no" +
+				"   and a.no=?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				result = new BookVo();
+				
+				result.setNo(rs.getLong(1));
+				result.setTitle(rs.getString(2));
+				result.setAuthorNo(rs.getLong(3));
+				result.setAuthorName(rs.getString(4));
+				result.setStateCode(rs.getString(5));
+			}
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection connection = null;
+		
+		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 			String url = "jdbc:mysql://192.168.10.38:3306/webdb?charset=utf8";
 			connection = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: "+ e );
+			System.out.println("드라이버 로딩 실패:" + e);
 		}
+		
 		return connection;
 	}
-
-
-
-
-
 }
